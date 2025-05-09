4466fa66e6caf41cadc5dec0a7ed59d667eea9b3
andrey.kerchin@gmail.com <andrey.kerchin@gmail.com>
2025-05-09 18:28:53 +0000

    Changes from 2025-05-09 18:28:53 by andrey.kerchin@gmail.com

--- aisaledev/companies-search-start-csharpCode.cs
+++ aisaledev/companies-search-start-csharpCode.cs
@@ -11,15 +11,24 @@ class Agent
         Func<string, string> GetCacheValue,
         Func<string, string, int, string> SetCacheValue)
     {
-        string companySearchGuid = Parameters["parameter1"]; 
-        string user = RequestAccessor.Login;
+        var companySearchGuid = Parameters["parameter1"]; 
+        var user = RequestAccessor.Login;
         
+        var companySearchData = new CompanySearchData();
+
+
         string sqlInsertQuery = $@"
-            INSERT INTO public.company_search (company_search_guid, executed_by, state, company_search_data)
-            VALUES ('{companySearchGuid}', '{user}', 'new', '{{}}')";
+            INSERT INTO public.company_search (company_search_guid, executed_by, state, company_search_data, last_message)
+            VALUES ('{companySearchGuid}', '{user}', 'new', '{{}}', 'Starting..')";
         
         var sqlResult = ExecuteAgent("sql-execute", new List<string> { sqlInsertQuery });
         
-        return sqlResult;
+        return ExecuteAgent("companies-search-get", new List<string> { companySearchGuid });
     }
+}
+
+class CompanySearchData
+{
+    public List<string> SearchQueries { get; set; } = new List<string>();
+
 }
