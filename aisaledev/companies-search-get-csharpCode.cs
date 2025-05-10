808ce6d8d580875f948c2b2340630cb574b63f59
andrey.kerchin@gmail.com <andrey.kerchin@gmail.com>
2025-05-10 09:57:08 +0000

    Changes from 2025-05-10 09:57:08 by andrey.kerchin@gmail.com

--- aisaledev/companies-search-get-csharpCode.cs
+++ aisaledev/companies-search-get-csharpCode.cs
@@ -16,13 +16,13 @@ class Agent
         var sqlSelectQuery = $@"select * from public.company_search where company_search_guid = '{companySearchGuid}' limit 1";
         var sqlResult = ExecuteAgent("sql-execute", new List<string> { sqlSelectQuery });
         
-        var parsedResult = JsonConvert.DeserializeObject<List<object>>(sqlResult);
+        var parsedResult = JsonConvert.DeserializeObject<List<List<object>>>(sqlResult);
         
-        if (parsedResult != null && parsedResult.Count > 0)
+        if (parsedResult != null && parsedResult.Count > 0 && parsedResult[0].Count > 0)
         {
-            var firstResult = parsedResult[0];
+            var firstResult = parsedResult[0][0];
             return JsonConvert.SerializeObject(firstResult);
         }        
-        return "Incorrect companySearchGuid"
+        return "Incorrect companySearchGuid";
     }
 }
