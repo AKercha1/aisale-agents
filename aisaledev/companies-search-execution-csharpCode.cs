50d2b5d5c52972e70d4d656f7b5cd9c673cd74a8
andrey.kerchin@gmail.com <andrey.kerchin@gmail.com>
2025-05-11 10:13:52 +0000

    Changes from 2025-05-11 10:13:52 by andrey.kerchin@gmail.com

---
+++ aisaledev/companies-search-execution-csharpCode.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+class Agent
+{
+    public string Run(
+        Dictionary<string, string> Parameters,
+        AiCoreApi.Common.RequestAccessor RequestAccessor,
+        AiCoreApi.Common.ResponseAccessor ResponseAccessor,
+        Func<string, List<string>?, string> ExecuteAgent,
+        Func<string, string> GetCacheValue,
+        Func<string, string, int, string> SetCacheValue)
+    {
+        return "Hello World!";
+    }
+}
