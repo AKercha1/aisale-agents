1e7b2e80623883904052aa0f0e126b411bee7087
andrey.kerchin@gmail.com <andrey.kerchin@gmail.com>
2025-05-11 10:24:14 +0000

    Changes from 2025-05-11 10:24:14 by andrey.kerchin@gmail.com

--- aisaledev/companies-search-execution-csharpCode.cs
+++ aisaledev/companies-search-execution-csharpCode.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 class Agent
 {
@@ -9,8 +10,50 @@ class Agent
         AiCoreApi.Common.ResponseAccessor ResponseAccessor,
         Func<string, List<string>?, string> ExecuteAgent,
         Func<string, string> GetCacheValue,
-        Func<string, string, int, string> SetCacheValue)
+        Func<string, string, int, string> SetCacheValue,
+        Microsoft.Extensions.Logging.ILogger<AiCoreApi.SemanticKernel.Agents.CsharpCodeAgent> Logger)
     {
-        return "Hello World!";
+        var companySearchGuid = Parameters["parameter1"];
+        var searchDepth = Convert.ToInt32(Parameters["parameter2"]);
+
+        var searchItemResult = ExecuteAgent("companies-search-get", new List<string> { companySearchGuid });
+        var companySearchItem = JsonConvert.DeserializeObject<CompanySearchItem>(searchItemResult);
+        var companySearchData = JsonConvert.DeserializeObject<CompanySearchData>(companySearchItem.company_search_data);
+        var searchQueriesCount = companySearchData.SearchQueries.Count;
+        var n = 0;
+        foreach (var searchQuery in companySearchData.SearchQueries)
+        {
+            n++;
+            companySearchItem.last_message = $"[{n}/{searchQueriesCount}] Handling '{searchQuery}' "
+            Save(companySearchItem, companySearchData, ExecuteAgent);
+
+        }
+        return searchItemResult;
     }
+
+    private void Save(CompanySearchItem companySearchItem, CompanySearchData companySearchData, Func<string, List<string>?, string> ExecuteAgent)
+    {
+        var updatedSearchDataJson = JsonConvert.SerializeObject(companySearchData);
+        companySearchItem.company_search_data = updatedSearchDataJson;
+        var updatedSearchItemJson = JsonConvert.SerializeObject(companySearchItem);
+        ExecuteAgent("companies-search-set", new List<string> { updatedSearchItemJson });
+    }
+}
+
+class CompanySearchItem
+{
+    public int company_search_id { get; set; }
+    public string company_search_guid  { get; set; }
+    public DateTime executed_at { get; set; }
+    public string executed_by { get; set; }
+    public string state { get; set; }
+    public string company_search_data  { get; set; }
+    public string last_message { get; set; }
+}
+
+class CompanySearchData
+{
+    public string QueryString { get; set; } = "";
+    public int? SearchDepth { get; set; }
+    public List<string> SearchQueries { get; set; } = new List<string>();
 }
