10853b861d2b0ac9268e14bfe820b4c1ac7e0fa6
andrey.kerchin@gmail.com <andrey.kerchin@gmail.com>
2025-05-12 11:45:56 +0000

    Changes from 2025-05-12 11:45:56 by andrey.kerchin@gmail.com

--- aisaledev/companies-add-csharpCode.cs
+++ aisaledev/companies-add-csharpCode.cs
@@ -11,6 +11,17 @@ class Agent
         Func<string, string> GetCacheValue,
         Func<string, string, int, string> SetCacheValue)
     {
+        var companySearchGuid = Parameters["parameter1"];
+        var companiesListJson = Parameters["parameter2"]; 
+        var companiesList = JsonConvert.DeserializeObject<List<CompaniesListItem>>(companiesListJson);
+
         return "Hello World!";
     }
+}
+
+class CompaniesListItem 
+{
+    public string name { get; set; }
+    public string url { get; set; }
+    public string details { get; set; }
 }
